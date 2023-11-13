5dd37c45553472458a1e98ce82c75cea52521cb7
AnnaLinneaW <annalinneawiklund@gmail.com>
2023-11-13 13:49:10 +0100

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
-// vite.config.js
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist', // eller 'build'
-  },
   plugins: [react()],
 });
