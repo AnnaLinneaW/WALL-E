5b931fe820dd35a11b7bdcfcd2d9bf676aae515d
AnnaLinneaW <annalinneawiklund@gmail.com>
2023-11-19 22:11:18 +0100

    root

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Outlet } from "react-router-dom";
-import { Navbar } from "../components/navbar";
-import { getCardUser } from "../redux/createCardSlice";
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import { Navbar } from '../components/navbar';
+import { getCardUser } from '../redux/createCardSlice';
 
 export const Root = () => {
   const dispatch = useDispatch();
